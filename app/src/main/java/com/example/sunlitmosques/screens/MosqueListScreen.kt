9c5d6fe4c01c434263dc99ee593ab8e9371ff92a
Louay Nasr <LouayNasr@users.noreply.github.com>
2024-07-19 17:49:48 +0300

    improve MosqueListScreen design

--- app/src/main/java/com/example/sunlitmosques/screens/MosqueListScreen.kt
+++ app/src/main/java/com/example/sunlitmosques/screens/MosqueListScreen.kt
@@ -1,13 +1,17 @@
+import android.content.res.Configuration
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -18,11 +22,14 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.example.sunlitmosques.Screen
 import com.example.sunlitmosques.models.Mosque
+import com.example.sunlitmosques.ui.theme.ChangaFontFamily
 import com.example.sunlitmosques.viewmodels.MosqueViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -33,7 +40,12 @@ fun MosqueListScreen(navController: NavController, mosqueViewModel: MosqueViewMo
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text("Sunlit Mosque") },
+                title = {
+                    Text(
+                        "Sunlit Mosque",
+                        fontFamily = ChangaFontFamily
+                    )
+                },
             )
         },
         content = { paddingValues ->
@@ -42,7 +54,7 @@ fun MosqueListScreen(navController: NavController, mosqueViewModel: MosqueViewMo
                     modifier = Modifier
                         .fillMaxSize()
                         .padding(paddingValues),
-                    contentPadding = PaddingValues(16.dp)
+                    contentPadding = PaddingValues(8.dp)
                 ) {
                     items(mosqueList) { mosque ->
                         MosqueRow(mosque) {
@@ -65,18 +77,57 @@ fun MosqueListScreen(navController: NavController, mosqueViewModel: MosqueViewMo
 
 @Composable
 fun MosqueRow(mosque: Mosque, onClick: () -> Unit) {
-    Row(
+    Card(
         modifier = Modifier
-            .fillMaxWidth()
             .clickable(onClick = onClick)
-            .padding(8.dp)
+            .padding(4.dp),
+        shape = RoundedCornerShape(4.dp),
+        colors = CardDefaults.cardColors()
     ) {
-        Column(modifier = Modifier.weight(1f)) {
-            Text(text = mosque.accountNumber, style = MaterialTheme.typography.bodyLarge)
-            Text(
-                text = "Total Outstanding: ${mosque.totalOutstanding}",
-                style = MaterialTheme.typography.bodyMedium
-            )
+        Row(
+            modifier = Modifier
+                .padding(8.dp)
+        ) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(
+                    text = mosque.accountNumber,
+                    fontFamily = ChangaFontFamily,
+                    style = MaterialTheme.typography.bodyLarge
+                )
+                Text(
+                    text = "Total Outstanding: ${mosque.totalOutstanding}",
+                    fontFamily = ChangaFontFamily,
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = if (isSystemInDarkTheme()) Color.Green else Color.DarkGray
+                )
+            }
         }
     }
+
+}
+
+
+val sampleMosque = Mosque(
+    accountNumber = "R21477G",
+    minName = "MINISTRY OF AWQAF (MOSQUE)(ELEC)",
+    totalOutstanding = 446.48,
+    monthlyConsumption = mapOf(
+        "22-Nov" to 446.477,
+        "22-Oct" to 0.0,
+        "22-Sep" to 0.0,
+        "22-Aug" to 0.0,
+        "22-Jul" to 0.0,
+        "22-Jun" to 0.0,
+        "22-May" to 0.0,
+        "22-Apr" to 0.0,
+        "22-Mar" to 0.0,
+        "22-Feb" to 0.0,
+        "22-Jan" to 0.0
+    )
+)
+
+@Preview(name = "Dark Mode", uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun PreviewMosqueItem() {
+    MosqueRow(mosque = sampleMosque, onClick = {})
 }
