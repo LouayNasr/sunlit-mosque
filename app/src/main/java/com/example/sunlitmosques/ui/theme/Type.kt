26cfd622b615f8581e099e3d8c4031ebc4a536ef
Louay Nasr <LouayNasr@users.noreply.github.com>
2024-07-19 15:40:47 +0300

    implement Changa font family

--- app/src/main/java/com/example/sunlitmosques/ui/theme/Type.kt
+++ app/src/main/java/com/example/sunlitmosques/ui/theme/Type.kt
@@ -2,9 +2,11 @@ package com.example.sunlitmosques.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.sunlitmosques.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -31,4 +33,11 @@ val Typography = Typography(
         letterSpacing = 0.5.sp
     )
     */
+)
+
+val ChangaFontFamily = FontFamily(
+    Font(R.font.changa_regular, FontWeight.Normal),
+    Font(R.font.changa_light, FontWeight.Light),
+    Font(R.font.changa_medium, FontWeight.Medium),
+    Font(R.font.changa_semibold, FontWeight.SemiBold)
 )
